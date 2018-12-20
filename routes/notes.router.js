d3c95d1fe3fff7e7a14e47f596d08e8463032d61
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-20 15:26:48 -0600

    Promisfied the notes.delete method within router.delete

--- routes/notes.router.js
+++ routes/notes.router.js
@@ -112,12 +112,22 @@ router.post('/notes', (req, res, next) => {
 router.delete('/notes/:id', (req, res, next) => {
   const id = req.params.id;
 
-  notes.delete(id, (err) => {
+  notes.delete(id)
+    .then(item => {
+      res.sendStatus(204);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+    
+/* Unpromisfied version
+    , (err) => {
     if (err) {
       return next(err);
     }
     res.sendStatus(204);
-  });
-});
+  }); 
+}); */
 
 module.exports = router;
