3af685cc61da31bfe7214b6e5d3200ba22c2623c
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-30 21:44:47 -0600

    Fixed express issue by (surprise) installing express. Also installed morgan.

--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,11 @@
+'use strict';
+const app = require('../server');
 const chai = require('chai');
+const chaiHttp = require('chai-http');
 
 const expect = chai.expect;
+
+chai.use(chaiHttp);
 
 describe('Reality check', function () {
 
